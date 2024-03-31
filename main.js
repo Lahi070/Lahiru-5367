850e7bc0d97978df0b7d07ed97d53c965ecdd174
Lahi070 <lahirusri070@gmail.com>
2024-04-01 00:34:51 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-// Swiper
-var swiper = new Swiper(".home", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+document.addEventListener("DOMContentLoaded", function() {
+  var mySwiper = new Swiper('.swiper', {
+      
+      loop: true, // Enable looping
+      navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+      },
+      autoplay: {
+          delay: 5000, // Set autoplay delay to 3 seconds (3000 milliseconds)
+          disableOnInteraction: false, // Continue autoplay even when user interacts with swiper
+      },
   });
+});
